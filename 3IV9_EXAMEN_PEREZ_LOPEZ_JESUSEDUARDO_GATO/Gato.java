47ed2fa2d130b9f7ef9b76cd38a36b6a6a1c9e26
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-24 04:53:07 -0600

    Método main

---
+++ 3IV9_EXAMEN_PEREZ_LOPEZ_JESUSEDUARDO_GATO/Gato.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Gato extends JFrame{
+
+    public Gato(){
+        setSize (3200, 3400);
+    }
+
+}
--- 3IV9_EXAMEN_PEREZ_LOPEZ_JESUSEDUARDO_GATO/Principal.java
+++ 3IV9_EXAMEN_PEREZ_LOPEZ_JESUSEDUARDO_GATO/Principal.java
@@ -1,10 +1,12 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Principal{
 
     public static void main(String[] args){
 
-        
+        Gato obj = new Gato();
+        obj.setVisible(true);
 
     }
 
